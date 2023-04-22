b07cf62fde7cf6627b092535bf5750451047e7d3
Steven Johnson <johnsonstheory@gmail.com>
2023-04-22 12:43:28 -0600

    updated config for clarity

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const wix_config = {
     //Site ID: can be found in the slug of the user's Dashboard
-    'wix-site-id': '',
+    'wix-site-id': 'xxxxxxxxx-xxxxxxxxx-xxxxxxxxx',
     //Account ID: found in Account > Account Settings > API Keys
-    'wix-account-id': '',
+    'wix-account-id': 'xxxxxxxxx-xxxxxxxxx-xxxxxxxxx',
     //Authorization: API Key generated through: Account > Account Settings > API Keys
-    Authorization: ''
+    Authorization: '[massive-token-here]'
 
 };
 
@@ -14,6 +14,6 @@ export const wp_config = {
     // The target of your website (must include /wp-json slug)
     endpoint: 'https://your_site.com/wp-json',
     // Assuming you're using basic auth
-    username: '',
-    password: ''
+    username: 'your-username',
+    password: 'your-password'
 };
